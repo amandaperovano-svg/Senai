38739d0988aa3bd33641de44e52fe9f3979c45d4
amandaperovano-svg <amanda_perovano@estudante.sesisenai.org.br>
2026-03-13 19:56:26 +0000

    update

--- atividade 1/programa4.py
+++ atividade 1/programa4.py
@@ -1,9 +1,20 @@
-nome: input("insira o nome do aluno:") # type: ignore
-nota1 = float(input("insira a primeiro nota:"))
-nota2 = float(input("insira a segundo nota:"))
-media = (nota1 + nota2) / 2 
-print("a media das notas é: ", media)
+contador = 1
+soma_notas = 0
+
+while contador <= 4:
+    notas = float(input(f"digite a nota do { contador } bimestre:"))
+    if notas < 0 or notas > 10: 
+        print("nota invalida. A nota deve estar entre 0 e 10")
+        continue
+    contador += 1
+    soma_notas += notas
+    
+media = soma_notas / 4
+print("a media de notas é: ", media)
+
 if media >= 7:
     print("o aluno esta aprovado")
+if media >= 5:
+    print("o aluno esta em recuperaçao")
 else:
     print("o aluno esta reprovado")
