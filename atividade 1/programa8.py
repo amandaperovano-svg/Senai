ee3a44836469000cca3fe9308d13df58ecd34dae
amandaperovano-svg <amanda_perovano@estudante.sesisenai.org.br>
2026-03-20 20:19:49 +0000

    update

--- atividade 1/programa8.py
+++ atividade 1/programa8.py
@@ -1,6 +1,9 @@
-nome = input("Digite seu nome:  ")
-idade = int(input("digite quantos anos vc tem: "))
+nome = input("Digite seu nome: ")
+idade = int(input("digite sua idade: "))
 
-
-idade = idade * 365
-print("vc ja viveu :", idade, "dias da sua vida")
+if idade > 120 or idade < 0:
+    print("idade inválida! Por favor, digite um valor menor ou igual a 120.")
+    print("idade inválida! Por favor, digite um valor maior que 0.")
+else:
+    dias_de_vida = idade * 365
+    print(f"Olá {nome}, voce já viveu cerca de: {dias_de_vida}")
