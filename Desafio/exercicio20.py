7c1f900e16411f47809e5e3ce08bf888f96ea509
amandaperovano-svg <amanda_perovano@estudante.sesisenai.org.br>
2026-05-08 19:03:47 +0000

    update

--- Desafio/exercicio20.py
+++ Desafio/exercicio20.py
@@ -3,4 +3,8 @@ qtd_frangos = int(input("Digite a quantidade de frangos"))
 custo_chip = 4.0
 custo_alimentos = 3.50
 
-gosto_por_frango = custo_chip + (2 * custo_alimentos)
+gasto_por_frango = custo_chip + (2 * custo_alimentos)
+gasto_total = qtd_frangos * gasto_por_frango
+
+print("A quantia de frangos na fazenda é: ", qtd_frangos)
+print("Para colocar a quantidade que deseja de aneis será preciso gastar: ", gasto_total)
